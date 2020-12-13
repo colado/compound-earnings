33d7852dded887eba90c79b87479667bd02ae026
Sergio Colado <sergiocoladoloro@gmail.com>
2020-12-13 16:52:51 +0100

    Set up screens

---
+++ src/screens/Allocation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const Allocation = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Allocation Scren</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Allocation;
---
+++ src/screens/Earnings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const Earnings = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Earnings Scren</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Earnings;
